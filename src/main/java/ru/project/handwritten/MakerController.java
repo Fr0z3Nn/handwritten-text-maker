1fd9f6107fb529f6fcca5b930c89b0c600c1b2f8
Fr0z3Nn <luckyjohn057@gmail.com>
2021-01-05 04:27:47 +0300

    Добавлено: Возможность выбора файла с помощью кнопки

--- src/main/java/ru/project/handwritten/MakerController.java
+++ src/main/java/ru/project/handwritten/MakerController.java
@@ -1,15 +1,24 @@
 package ru.project.handwritten;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import ru.project.handwritten.entity.DOCXDocument;
 import ru.project.handwritten.logger.Logger;
 import ru.project.handwritten.util.writeUtil;
 import ru.project.handwritten.validator.InputValidator;
 
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 
 public class MakerController {
@@ -34,10 +43,19 @@ public class MakerController {
 
     public static TextArea textAreaLOG;
 
+    // класс позволяющий выбирать файл
+    final FileChooser fileChooser = new FileChooser();
+
+    @FXML
+    public Button chooseFile;
+
     @FXML
     public void initialize() {
         //присваиваем нашему логеру поле техареа с которым он будет работать
         textAreaLOG = textArea;
+        //название для окошка выбора файла
+        fileChooser.setTitle("Выбор файла для генерации");
+
         generation.setOnMouseClicked(event -> {
             InputValidator inputParams = InputValidator.builder()
                     .path(inputPath.getText())
@@ -64,9 +82,17 @@ public class MakerController {
             }
 
             Logger.logADD("\nФайл успешно сгенерирован!\n");
-
-
         });
+
+        chooseFile.setOnAction(event -> {
+            textArea.clear();
+            Stage window = new Stage();
+            File file = fileChooser.showOpenDialog(window);
+            if (file != null) {
+                inputPath.setText(file.getAbsolutePath());
+            }
+        });
+
     }
 
 
